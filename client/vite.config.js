065016fa4d404bcee863a653c3aa70eff1d3fd38
Pavan <153820912+Pavan-2929@users.noreply.github.com>
2024-01-06 22:47:05 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://auth-server-jixq.onrender.com", // Use http instead of https
-        secure: true, // Add this line to disable SSL verification
-      },
-    },
-  },
   plugins: [react()],
 });
